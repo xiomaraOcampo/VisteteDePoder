9bd6c227639a58fa0701d5aaa3e9200072f9f4ce
florcadelli <florcadelli@gmail.com>
2020-11-11 20:52:41 -0300

    multer a medio camino

--- trabajo integrador/routes/products.js
+++ trabajo integrador/routes/products.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var productController = require ('../controllers/productController');
+/*var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+const path = require ('path');
 
-//ruta para el detyalle de productos que ve el usuario
+// multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+  var upload = multer({ storage: storage })*/
+
+//ruta para el detalle de productos que ve el usuario
 
 router.get('/detailProduct',productController.detail);
 
 //ruta para los formularios del administrador, para cargar productos
 
 router.get('/create',productController.create);
-router.post('/create',productController.store);
+router.post('/create',/*upload.single('avatar'),*/productController.store);
 
 //ruta para los formularios del administrador, para modificar productos
 
