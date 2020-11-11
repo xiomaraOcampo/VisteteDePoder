be59921a42fd83462c711a34016f0b195ae9b737
Laulore86 <lauraloreiro1@gmail.com>
2020-11-11 17:18:59 -0300

    Armando Funciones del formulario

--- trabajo integrador/controllers/productController.js
+++ trabajo integrador/controllers/productController.js
@@ -1,7 +1,16 @@
+const fs = require ('fs');
+//fs.writeFileSync(__dirname + '/../Data/productsFile.json' , []);
+let productsFile= JSON.parse(fs.readFileSync(__dirname + '/../Data/productsFile.json' ,{encoding: "utf-8"}));
+
+//let productsJson= JSON.parse(productsFile);
+
+
+
+
 let productController = {
 
-    producto: function(req, res, next) {
-        res.render('productsViews/producto');
+    detail: function(req, res, next) {
+        res.render('productsViews/detailProducts');
       }, 
       //esta función es para el formulario de creación de productos// 
     
@@ -9,8 +18,23 @@ let productController = {
       res.render('productsViews/create');
     }, 
     store: function(req, res, next) {
-      res.send('productooo');
-    }, 
+    let product= req.body; 
+    productsFile.push(product);
+    let productsFileJson= JSON.stringify(productsFile);
+    fs.writeFileSync(__dirname + '/../Data/productsFile.json' , productsFileJson);
+
+     res.send('agregaste un producto ' + req.body.nombre);
+     
+    }
+    //edit: function(req, res, next){
+      //var idProduct= req.params.id;
+      //var productFound;
+      //for (var i=0; i <productsFile.length; i++){
+
+       // if(productsFile[i].id == idProduct{})
+     // }
+       // console.log(idProduct);
+    //}
   
     }
       module.exports=productController;
--- trabajo integrador/routes/products.js
+++ trabajo integrador/routes/products.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 var productController = require ('../controllers/productController');
 
+//ruta para el detyalle de productos que ve el usuario
 
-router.get('/producto',productController.producto);
+router.get('/detailProduct',productController.detail);
 
+//ruta para los formularios del administrador, para cargar productos
 
 router.get('/create',productController.create);
+router.post('/create',productController.store);
+
+//ruta para los formularios del administrador, para modificar productos
+
+//router.get('/edit/:id',productController.edit);
+//router.post('/edit/:id',productController.update);
+
 
 module.exports = router;
