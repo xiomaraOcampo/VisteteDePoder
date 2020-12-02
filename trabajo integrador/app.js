1fb98563051642c88669871d0dba092419f92bad
Laulore86 <lauraloreiro1@gmail.com>
2020-12-02 19:25:37 -0300

    con session descomentado

--- trabajo integrador/app.js
+++ trabajo integrador/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
-//var session=require('express-session');
+var session=require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
-//app.use(session({secret:'secreto'}));
+app.use(session({secret:'secreto'}));
 
 
 app.use('/', indexRouter);
