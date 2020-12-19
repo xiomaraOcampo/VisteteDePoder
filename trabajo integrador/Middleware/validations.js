4819856d95428d2c4548cd452788e8970552fe9e
Laulore86 <lauraloreiro1@gmail.com>
2020-12-19 08:45:28 -0300

    validaciones de creación de productos

--- trabajo integrador/Middleware/validations.js
+++ trabajo integrador/Middleware/validations.js
@@ -16,8 +16,9 @@ let {check,validationResult,body}= require('express-validator');
     ],
 
     productCreated:[
-      check ('nombre').isLength({min:1}).withMessage('El campo nombre debe estar completo')
-     // check ('descripcion').isLength({min:1}).withMessage('Este campo debe estar completo')
+      check ('nombre').isLength({min:1}).withMessage('El campo nombre debe estar completo'),
+      check ('descripcion').isLength({min:1}).withMessage('Este campo debe estar completo'),
+      check ('precio').isCurrency({symbol: '$', require_symbol: false, allow_space_after_symbol: false, symbol_after_digits: false, allow_negatives: true, parens_for_negatives: false, negative_sign_before_digits: false, negative_sign_after_digits: false, allow_negative_sign_placeholder: false, thousands_separator: '.', decimal_separator: ',', allow_decimal: true, require_decimal: false, digits_after_decimal: [2], allow_space_after_digits: false}).withMessage('El campo precio debe estar completo')
 
 
     ]
