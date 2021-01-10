b01801081b166d49151d4428ece5f28f1da29e1a
Xiomara Ocampo <xiomaravictoriaocampo@gmail.com>
2021-01-10 12:14:11 -0300

    Subo como estaba el viernes

--- trabajo integrador/database/config/config.js
+++ trabajo integrador/database/config/config.js
@@ -2,7 +2,7 @@ module.exports= {
   "development": {
     "username": "root",
     "password": null,
-    "database": "dbvistete",
+    "database": "database_vistetep",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
