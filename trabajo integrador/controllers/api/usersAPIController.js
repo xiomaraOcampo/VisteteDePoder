91054c5932186cf5156f0b3d795373bf4b3c3040
Xiomara Ocampo <xiomaravictoriaocampo@gmail.com>
2021-02-01 17:50:36 -0300

    API de users

--- trabajo integrador/controllers/api/usersAPIController.js
+++ trabajo integrador/controllers/api/usersAPIController.js
@@ -5,21 +5,15 @@ const db = require("../../database/models");
 let usersAPIController = {
   
 
-list: function (req, res, next) {
+ list: function (req, res, next) {
     db.User.findAll()
-    .then(function (respuesta) {
-      // res.render('usersViews/uList', { usersFile: result});
+    .then(function (usuarios) {
       let respuesta=usuarios;
-      return res.json(respuesta);
+       res.send(respuesta);
     })
-
-    // .catch(function (error) {
-    //   console.log(error)
-    //   res.send("Error")
-    // });
   
-}
+  }
 
-};//cierre controller
+};
 
 module.exports = usersAPIController;
