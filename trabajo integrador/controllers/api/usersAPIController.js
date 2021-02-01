27fb60036e4974f5f192e256a728cfa301bef51c
Xiomara Ocampo <xiomaravictoriaocampo@gmail.com>
2021-02-01 18:58:45 -0300

    subo l meta de User

--- trabajo integrador/controllers/api/usersAPIController.js
+++ trabajo integrador/controllers/api/usersAPIController.js
@@ -4,12 +4,21 @@ const db = require("../../database/models");
 
 let usersAPIController = {
   
-
  list: function (req, res, next) {
     db.User.findAll()
-    .then(function (usuarios) {
-      let respuesta=usuarios;
-       res.send(respuesta);
+    .then(function (users){
+      for (let i=0; i<users.length; i++){
+        users[i].setDataValue("endpoint", "/api/user/" +users[i].id)
+      }
+      let respuesta={
+        meta:{
+          status:200,
+          total:users.length,
+          url:"/api/users"
+        },
+        data:users
+      };
+       res.json(respuesta);
     })
   
   }
