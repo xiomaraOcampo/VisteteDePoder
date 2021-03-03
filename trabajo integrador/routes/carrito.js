d6bc1f0e434f87bfb5cc7d9dd9b18f7d712b9715
xiomaraOcampo <xiomaravictoriaocampo@gmail.com>
2021-03-03 08:36:31 -0300

    botones carrito 3

--- trabajo integrador/routes/carrito.js
+++ trabajo integrador/routes/carrito.js
@@ -12,14 +12,9 @@ router.get('/carrito2',authMiddleware, carritoController.carrito2 );
 //Empiezo el CRUD de carrito,vamos que se puede !!
 
 router.get('/',authMiddleware,carritoController.cargaCarrito );
-<<<<<<< HEAD
 router.post('/agregarProducto/:id', carritoController.agregarProducto); 
 router.delete('/destroy/:id', carritoController.destroy); 
 router.post('/vaciar/:id', carritoController.vaciarCarrito);
-=======
-router.post('/agregarProducto/:id',authMiddleware, carritoController.agregarProducto); 
-router.post('/vaciar',authMiddleware, carritoController.vaciarCarrito);
->>>>>>> dceb4ae1b16417375bbeacb0e21c1097064473e6
 
 
 module.exports = router;
